3450cd42e2a727070838707e044551017ced689f
Dante Doig-Acuna <doig.acuna@gmail.com>
2018-08-16 22:08:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+
+// define `drivers` array 
+const drivers = ["Milo", "Otis", "Garfield"]
+
+// append new driver to end of array
+function destructivelyAppendDriver(Ralph) {
+  (drivers.push("Ralph"))
+}
+
+// prepend new driver to start of array
+function destructivelyPrependDriver(Bob) {
+  (drivers.unshift("Bob"))
+}
+
+// remove driver from end of array
+function destructivelyRemoveLastDriver() {
+  (drivers.pop())
+}
+
+// remove driver from start of array
+function destructivelyRemoveFirstDriver() {
+  (drivers.shift())
+}
+
+
+
